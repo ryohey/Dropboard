08ed6c3d10d2e012ff873425235a4c8f555a0d85
ryocc <r.k.blvdr@gmail.com>
2012-11-11 12:08:16 +0900

    ファイルを追加するようにした

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ var app = express();
 
 app.use(require('connect').bodyParser());
 
-app.post("/post", function(req, res){
+var getFiles = function(dataPath){
+    var files = fs.readdirSync(dataPath);
+    var list = [];
+    files.forEach(function(fileName){
+        if (fileName.match(/.+\.json/)){
+            var file = fs.readFileSync(dataPath+fileName);
+            list.push(JSON.parse(file));
+        }
+    });
+    return list;
+};
+
+app.post("/write", function(req, res){
     var data = req.body;
     if (typeof data.name !== undefined && typeof data.date !== undefined && typeof data.text !== undefined){
         fs.writeFile('./data/'+data.name+"「"+data.text+"」"+'.json', JSON.stringify(data), function(err){
@@ -17,29 +29,8 @@ app.post("/post", function(req, res){
     }
 })
 
-app.post("/write", function(req, res){
-    var data;
-    try{
-        var file = fs.readFileSync('./data.json');
-        data = JSON.parse(file);
-    }catch(e){
-        console.log(e);
-        data = [];
-    }
-    console.log(data,req.body);
-    if (typeof req.body.name !== undefined && typeof req.body.date !== undefined && typeof req.body.text !== undefined){
-        data.push(req.body);
-        fs.writeFile('./data.json', JSON.stringify(data), function(err){
-            if (err)
-                res.send("0");
-            else
-                res.send("1");
-        });
-    }
-})
-
 app.get('/read', function(req, res){
-    res.send(fs.readFileSync('./data.json'));
+    res.send(JSON.stringify(getFiles('./data/')));
 });
 
 app.use("/",express.static(__dirname + '/public'));
--- start.command
+++ start.command
@@ -3,3 +3,4 @@ CURDIR=`dirname $0`
 cd $CURDIR
 killall node_
 ./node_ server.js & open http://localhost:3141/
+read WAIT
