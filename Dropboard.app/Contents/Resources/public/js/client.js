76c1a51fbe7002e85ceac899fdf4db97b25fc977
ryocc <r.k.blvdr@gmail.com>
2012-12-15 17:56:55 +0900

    なんか

--- Dropboard.app/Contents/Resources/public/js/client.js
+++ Dropboard.app/Contents/Resources/public/js/client.js
@@ -3,129 +3,129 @@ var lbNotify = new LBNotify;
 
 /* 発言フィードを生成 */
 var messageHTML = function(data) {
-  var date = new Date(data.date);
-  var datestr = naturalFormatDate(date);
-  var fileHTML = $("<div/>").addClass("file");
-  if (data.file) {
-    try{
-      var files = JSON.parse(data.file);
-      $.each(files,function(val,key){
-        var elm = $("<a/>")
-              .attr("target","_blank")
-              .attr("href",key);
-        if (isImage(key)){
-          elm
-            .fancybox()
-            .append(
-              $("<img/>").attr("src",key)
-            )
-        }else if (isAudio(key)){
-          //リンクはいらないのでelmを上書き
-          elm = $("<audio/>")
-            .attr("src",key)
-            .attr("controls","controls");
-        }else{
-          var fileName = parseURL(key).fileName;
-          elm.addClass("otherfile").text(fileName);
-        }
-        fileHTML.append(elm);
-      })
-    }catch(e){
+	var date = new Date(data.date);
+	var datestr = naturalFormatDate(date);
+	var fileHTML = $("<div/>").addClass("file");
+	if (data.file) {
+		try{
+			var files = JSON.parse(data.file);
+			$.each(files,function(val,key){
+				var elm = $("<a/>")
+							.attr("target","_blank")
+							.attr("href",key);
+				if (isImage(key)){
+					elm
+						.fancybox()
+						.append(
+							$("<img/>").attr("src",key)
+						)
+				}else if (isAudio(key)){
+					//リンクはいらないのでelmを上書き
+					elm = $("<audio/>")
+						.attr("src",key)
+						.attr("controls","controls");
+				}else{
+					var fileName = parseURL(key).fileName;
+					elm.addClass("otherfile").text(fileName);
+				}
+				fileHTML.append(elm);
+			})
+		}catch(e){
 
-    }
+		}
     }
 
-  var target =  $("<article/>")
-      .addClass("box")
-      .css("display", "none")
-    .append(
-      $("<header/>")
-        .append(
-          $("<a/>")
-            .attr("rel","author")
-            .text(data.name)
-        )
-        .append(
-          $("<time/>")
-            .text(datestr)
-        )
-    )
-    .append(
-      $("<p/>")
-        .append(formatMessage(data.text))
-        .append(fileHTML)
-    );
-  return target;
+	var target =  $("<article/>")
+	    .addClass("box")
+	    .css("display", "none")
+		.append(
+			$("<header/>")
+				.append(
+					$("<a/>")
+						.attr("rel","author")
+						.text(data.name)
+				)
+				.append(
+					$("<time/>")
+						.text(datestr)
+				)
+		)
+		.append(
+			$("<p/>")
+				.append(formatMessage(data.text))
+				.append(fileHTML)
+		);
+	return target;
 }
 
 /* 全消去 */
 var clear = function(){
-  $("#content").html("");
+	$("#content").html("");
 }
 
 /* アップデート処理 */
 var update = function(){
-  lbAjax.update(function(data){
-    $.each(data,function(){
-      messageHTML(this).prependTo("#content").show("slow");
-    })
-  })
+	lbAjax.update(function(data){
+		$.each(data,function(){
+			messageHTML(this).prependTo("#content").show("slow");
+		})
+	})
 }
 
 /* 古いデータを取ってくる */
 var more = function(){
-  lbAjax.more(function(data){
-    data.reverse();
-    $.each(data,function(){
-      messageHTML(this).appendTo("#content").show("slow");
-    })
-  })
+	lbAjax.more(function(data){
+		data.reverse();
+		$.each(data,function(){
+			messageHTML(this).appendTo("#content").show("slow");
+		})
+	})
 }
 
 /* 書き込むボタン無効化 */
 var disableWriteButton = function() {
-  $("#write").attr("disabled", true);
-  $("#write").addClass("disable");
+	$("#write").attr("disabled", true);
+	$("#write").addClass("disable");
 }
 
 /* 書き込むボタン有効化 */
 var enableWriteButton = function() {
-  $("#write").attr("disabled", false);
-  $("#write").removeAttr("disabled");
-  $("#write").removeClass("disable");
+	$("#write").attr("disabled", false);
+	$("#write").removeAttr("disabled");
+	$("#write").removeClass("disable");
 }
 
 /* 「書き込む」ボタン押し下げ時 */
 var writeButton = function() {
-  disableWriteButton();
-  var files = $("#text").data("files");
-  if (files)
-    lbAjax.upload(files,function(response){
-      console.log(response);
-      $("#text")
-        .data("files",null)
-            .removeClass("attached");
-          $("#files").html("");
-      lbAjax.write({
-        name:$("#name").val(),
-        date:new Date(),
-        text:$("#text").val(),
-        file:response
-      },function(){
-        $("#text").val("");
-      });
-    });
-  else
-    lbAjax.write({
-      name:$("#name").val(),
-      date:new Date(),
-      text:$("#text").val()
-    },function(){
-      $("#text").val("");
-    });
+	disableWriteButton();
+	var files = $("#text").data("files");
+	if (files)
+		lbAjax.upload(files,function(response){
+			console.log(response);
+			$("#text")
+				.data("files",null)
+	        	.removeClass("attached");
+	        $("#files").html("");
+			lbAjax.write({
+				name:$("#name").val(),
+				date:new Date(),
+				text:$("#text").val(),
+				file:response
+			},function(){
+				$("#text").val("");
+			});
+		});
+	else
+		lbAjax.write({
+			name:$("#name").val(),
+			date:new Date(),
+			text:$("#text").val()
+		},function(){
+			$("#text").val("");
+		});
 
-  // 名前のクッキーを焼く
-  $.cookie('name', $("#name").val(), {expires: 30});
+	// 名前のクッキーを焼く
+	$.cookie('name', $("#name").val(), {expires: 30});
 
     // ファイル情報削除
     $('#files').empty();
@@ -135,54 +135,54 @@ var writeButton = function() {
 
 // onload
 $(function(){
-  //通知を追加
-  lbNotify.elm.appendTo("body");
-  lbNotify.setPosition("bottom");
+	//通知を追加
+	lbNotify.elm.appendTo("body");
+	lbNotify.setPosition("bottom");
 
-  // 「書き込む」ボタン
-  $("#write").click(writeButton);
-  disableWriteButton();
+	// 「書き込む」ボタン
+	$("#write").click(writeButton);
+	disableWriteButton();
+	
+	// Ctrl+Enterで送信
+	$(window).keydown( function(e) {
+		if (e.ctrlKey && e.keyCode == 13)  {
+			if (document.activeElement.id == 'text') {
+				if ($("#text").val() != "") {
+					writeButton();
+				}
+			}
+			event.preventDefault();
+		}
+	});
+	
+	// textareaの監視
+	$("#text")
+		.bind('keyup change', function() {
+			if ($(this).val() == "") {
+				disableWriteButton();
+			} else {
+				enableWriteButton();
+			}
+		})
+		.bind("drop", function (e) {
+	        // ドラッグされたファイル情報を取得
+	        var files = e.originalEvent.dataTransfer.files;
+	        console.log(files);
+	        $("#files").html("");
+	        $.each(files,function(){
+	        	$("#files").append($("<li/>").text(this.name));
+	        });
+	        $(this).data("files",files);
+	        $(this).addClass("attached");
+	        e.preventDefault(); 
+	        e.stopPropagation();
+	    });
 
-  // Ctrl+Enterで送信
-  $(window).keydown( function(e) {
-    if (e.ctrlKey && e.keyCode == 13)  {
-      if (document.activeElement.id == 'text') {
-        if ($("#text").val() != "") {
-          writeButton();
-        }
-      }
-      event.preventDefault();
-    }
-  });
+	// クッキー
+	$("#name").val($.cookie('name'));
 
-  // textareaの監視
-  $("#text")
-    .bind('keyup change', function() {
-      if ($(this).val() == "") {
-        disableWriteButton();
-      } else {
-        enableWriteButton();
-      }
-    })
-    .bind("drop", function (e) {
-          // ドラッグされたファイル情報を取得
-          var files = e.originalEvent.dataTransfer.files;
-          console.log(files);
-          $("#files").html("");
-          $.each(files,function(){
-            $("#files").append($("<li/>").text(this.name));
-          });
-          $(this).data("files",files);
-          $(this).addClass("attached");
-          e.preventDefault();
-          e.stopPropagation();
-      });
-
-  // クッキー
-  $("#name").val($.cookie('name'));
-
-  // goTop
-  var topBtn = $('#goTop');
+	// goTop
+	var topBtn = $('#goTop');   
     topBtn.hide();
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -216,17 +216,17 @@ $(function(){
         //開く
         else if (openFlag == false) {
             slide
-              .stop()
-              .animate({
-                'width' : '400px',
-                'height' : '190px'
-              }, 300
-              ,function(){
-                contents.show();
-                $("#text").focus();
-              });
+            	.stop()
+            	.animate({
+            		'width' : '400px',
+            		'height' : '190px'
+            	}, 300
+            	,function(){
+            		contents.show();
+            		$("#text").focus();
+            	});
             openDiv.stop().animate({'top' : '182px','right' : '390px'}, 300);
-
+            
             openDiv.addClass("close");
             openFlag = true;
         }
@@ -241,9 +241,10 @@ $(function(){
     //下まで来たらもっと読み込む
     $(window).bottom();
     $(window).bind("bottom", function() {
-      more();
+    	more();
     });
 
-   // 更新の設定
+ 	// 更新の設定
     update();
+    setInterval(update,5000);
 })
